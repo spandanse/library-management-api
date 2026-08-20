dc3c981d2784422a191ac73edd7f760d0f4ad339
Spandan Sen <spandansen11a@gmail.com>
2026-08-20 17:28:11 +0530

    feat: add roles migration

---
+++ migrations/20260820115515-create-roles.js
@@ -0,0 +1,31 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("roles", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("roles");
+  }
+};
